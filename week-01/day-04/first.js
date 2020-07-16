2c90262a16a7c11bd229738cd8e60e79d5cf8008
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-16 10:41:27 +0200

    creating js and ts file for practicing

---
+++ week-01/day-04/first.js
@@ -0,0 +1,13 @@
+/*
+let x = 5;
+
+console.log(`Hello` + x)
+console.log (x)
+*/
+
+const x = 5;
+let y = 10;
+y = 20;
+let oldEnoughToDrink = false;
+console.log (`Hello ` + x);
+console.log(`first?`, x, y);
---
+++ week-01/day-04/second.ts
@@ -0,0 +1,15 @@
+/*
+let x = 5;
+
+console.log(`Hello` + x)
+console.log (x)
+*/
+
+const a:number =5;
+let b:number = 10;
+y = 20;
+let oldEnoughToDrive:boolean = false;
+console.log (`Hello ` + x);
+console.log(`first?`, x, y);
+console.log(17 % 5);
+console.log(17 / 5);
