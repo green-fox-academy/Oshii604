0a59bc6113366b56c225b0f1f9f298b151ea52f4
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-16 17:15:20 +0200

    second.ts changed

--- week-01/day-04/second.ts
+++ week-01/day-04/second.ts
@@ -5,6 +5,7 @@ console.log(`Hello` + x)
 console.log (x)
 */
 
+/*
 const a:number =5;
 let b:number = 10;
 y = 20;
@@ -13,3 +14,25 @@ console.log (`Hello ` + x);
 console.log(`first?`, x, y);
 console.log(17 % 5);
 console.log(17 / 5);
+*/
+
+//for (let i = 0; i< 100; i++) {
+//    console.log(`I won't cheat on the exam`)
+//}
+
+for (let i = 0; i< 100; i++) {
+    if(i % 2 === 0){
+        console.log(i + `th time`)
+        console.log(`I won't cheat on the exam`)
+    }
+}
+
+let i = 42;
+while ( i < 50) {
+
+
+}
+
+do {
+    //repeating sthg
+} while (i < 50)
