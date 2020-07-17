a8f90409b46d358123dd0dbd154a460104c3bbba
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-17 15:28:08 +0200

    loops.ts added

---
+++ week-01/day-04/loops.ts
@@ -0,0 +1,117 @@
+`use strict`
+
+/*
+for(let i: number = 0; i < 100; i++) {
+    console.log(`I won't cheat on the exam!`);
+    
+}
+
+
+for(let i: number = 0; i < 501; i++){
+    if(i % 2 == 0){
+        console.log(i);
+        
+    }
+}
+
+
+
+let number: number = 15;
+
+for(let i: number = 1; i <= 10; i++){
+    console.log(i + ` * ` + number + ` = ` + number * i);
+    
+}
+
+
+
+for(let i = 0; i < 8; i++ ){
+    if()
+}
+
+
+let end: number = 9;
+let sum: number = 0;
+
+for (let i: number = 1; i<=end; ++i){
+  sum += i;
+}
+
+let avg: number = sum / end;
+
+console.log(`Sum: ` + sum + ` Average: ` + avg);
+
+
+
+
+
+for(let i :number = 1; i < 101; i++){
+
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log(`FizzBuzz`);
+    }
+    else if(i % 3 == 0){
+        console.log(`Fizz`);
+    }
+    else if(i % 5 == 0){
+        console.log(`Buzz`);
+    }
+    else{
+        console.log(i); 
+    }
+}
+
+
+let lineCount: number = 4;
+let star: string = `*`;
+let drawing: string = ``
+
+for(let i: number = 1; i <= lineCount; i++){
+    drawing += star;
+    console.log(drawing);
+    
+}
+
+
+let lineCount: number = 4;
+let star: string = `*`;
+let space: string = ` `;
+
+
+for(let i: number = 1; i <= lineCount; i++){
+    for(let j: number = lineCount - i; j >= 0; j--){
+        space = space + ` `
+    }
+    console.log(space, star);
+    space = " ";
+    star += `**`;
+    
+}
+*/
+
+
+let lineCount1: number = 7;
+
+let star2: string="";
+
+for (let i = 0; i < lineCount1; i++) {
+
+
+    let spaceLength: number =lineCount1-i;
+    let spacePrefix: string="";
+    while(spaceLength>0){
+        spacePrefix += " ";
+        spaceLength--;
+    }
+
+
+    if (i==0){
+        star2= star2 + "*";
+    }else{
+        star2+="**";
+    }
+    console.log(spacePrefix+ star2);
+
+
+
+}
