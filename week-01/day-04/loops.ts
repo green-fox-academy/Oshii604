0c3cf4d461b977af45a213ba4e90921b0fda3468
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-17 15:29:46 +0200

    loops.ts modified

--- week-01/day-04/loops.ts
+++ week-01/day-04/loops.ts
@@ -89,29 +89,3 @@ for(let i: number = 1; i <= lineCount; i++){
 }
 */
 
-
-let lineCount1: number = 7;
-
-let star2: string="";
-
-for (let i = 0; i < lineCount1; i++) {
-
-
-    let spaceLength: number =lineCount1-i;
-    let spacePrefix: string="";
-    while(spaceLength>0){
-        spacePrefix += " ";
-        spaceLength--;
-    }
-
-
-    if (i==0){
-        star2= star2 + "*";
-    }else{
-        star2+="**";
-    }
-    console.log(spacePrefix+ star2);
-
-
-
-}
