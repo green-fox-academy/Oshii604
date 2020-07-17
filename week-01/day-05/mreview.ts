6878c5dae5d754b9d8c8045dcd3bda27ccec9cb3
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-17 12:43:22 +0200

    mreview.ts added

---
+++ week-01/day-05/mreview.ts
@@ -0,0 +1,15 @@
+'use strict'
+
+//Functions
+
+function add(x:number, y:number):number {
+    console.log(x);
+    console.log(y);
+    console.log(x+y);
+    
+    return x + y;
+    
+}
+
+//let addedNumbers: number = add(x: 4, y: 5);
+
