91c323b7c70b51c02dae8ace9a62036d1d6cbd53
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-21 13:35:07 +0200

    progressing in strings.ts

--- week-02/day-02/strings.ts
+++ week-02/day-02/strings.ts
@@ -57,11 +57,12 @@ function reverse(x:string){
 console.log(reverse(toBeReversed));
 }
 
-//URL-fixer
+//URL-fixer - jó, mostantól megyek a legegyszerűbb úton :)
 
 // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
 // Also, the URL is missing a crucial component, find out what it is and insert it too!
 
 let url: string = 'https//www.reddit.com/r/nevertellmethebots';
-
+url = url.replace(`https//`, `https://`)
+url = url.replace(`bots`, `odds`)
 console.log(url);
