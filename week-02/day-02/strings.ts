08b7b10658d92294a32306f2c2ac0cf0464b2c77
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-21 13:52:21 +0200

    progressing in strings.ts

--- week-02/day-02/strings.ts
+++ week-02/day-02/strings.ts
@@ -66,3 +66,14 @@ let url: string = 'https//www.reddit.com/r/nevertellmethebots';
 url = url.replace(`https//`, `https://`)
 url = url.replace(`bots`, `odds`)
 console.log(url);
+
+
+//takes longer
+
+// While saving this quote, a disk error has occured. Please fix it.
+// Add "always takes longer than" between the words "It" and "you"
+
+let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
+quote = [quote.slice(0, 20) + ` always takes longer than` + quote.slice(20)].join(``)
+
+console.log(quote);
