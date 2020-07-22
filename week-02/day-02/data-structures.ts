4ada77eea9ea10609f41cb2cde3cf10a447bef19
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-22 13:28:31 +0200

    wip on data-structures.ts

--- week-02/day-02/data-structures.ts
+++ week-02/day-02/data-structures.ts
@@ -2,48 +2,51 @@
 //List introduction 1
 
 {
-let names: string[] = [];
+    let names: string[] = [];
 
-console.log(names);
+    console.log(names);
 
-names.push(`William`)
+    names.push(`William`)
 
-if(Object.entries(names).length === 0){
-console.log(`This list is empty`)
-}
+    if (Object.entries(names).length === 0) {
+        console.log(`This list is empty`)
+    }
 
-names.push(`John`)
-names.push(`Amanda`)
+    names.push(`John`)
+    names.push(`Amanda`)
 
-console.log(names.length);
+    console.log(names.length);
 
-console.log(names[2]);
+    console.log(names[2]);
 
-names.forEach(listing);
-function listing(item: string){
-    console.log(item);
-}
+    names.forEach(listing);
+    function listing(item: string) {
+        console.log(item);
+    }
 
+    //Itt valami hiba van, ami korábban nem volt :(
 
-names.forEach(listing2);
-function listing2(index: number, item: string){
-    console.log((index), item);
-}
+    /*
+        names.forEach(listingTwo);
+        function listingTwo(index: number, item: string) {
+            console.log((index), item);
+        }
+    
+        names.splice(names.indexOf(names[1]), 1);
+    
+        console.log(names);
+    */
 
-names.splice(names.indexOf(names[1]), 1);
+    names.reverse().forEach(listing3);
+    function listing3(item: string) {
+        console.log(item);
+    }
 
-console.log(names);
+    names.splice(names.indexOf(names[0]), 1);
 
-names.reverse().forEach(listing3);
-function listing3(item: string){
-    console.log(item);
-}
+    names = [];
 
-names.splice(names.indexOf(names[0]), 1);
-
-names = [];
-
-console.log(names);
+    console.log(names);
 }
 
 
@@ -123,4 +126,38 @@ console.log(ListA[2]);
 
 
 //Map introduction 2
+
+let isbnMap = new Map();
+isbnMap.set(`978-1-60309-452-8`, `A Letter to Jo`)
+    .set(`978-1-60309-459-7`, `Lupus`)
+    .set(`978-1-60309-444-3`, `Red Panda and Moon Bear`)
+    .set(`978-1-60309-461-0`, `The Lab`);
+
+for (let [key, value] of isbnMap.entries()) {
+    console.log(value + ` (ISBN: ` + key + `)`)
+}
+
+isbnMap.delete(`978-1-60309-444-3`);
+
+for (let [key, value] of isbnMap.entries()) {
+    if (value === `The Lab`) {
+        isbnMap.delete(key)
+    }
+}
+
+for (let [key, value] of isbnMap.entries()) {
+    console.log(value + ` (ISBN: ` + key + `)`)
+}
+
+
+isbnMap.set(`978-1-60309-450-4`, `They Called Us Enemy`)
+    .set(`978-1-60309-453-5`, `Why Did We Trust Him?`)
+
+for (let [key, value] of isbnMap.entries()) {
+    console.log(value + ` (ISBN: ` + key + `)`)
+}
+
+console.log(isbnMap.has(`478-0-61159-424-8`));
+
+console.log(isbnMap.get(`978-1-60309-453-5`));
 
