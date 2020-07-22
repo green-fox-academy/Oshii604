bdc2a355e0ab55a8ef2caca55dcd4e37e72cb41f
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-22 07:10:45 +0200

    progress in data-structures.ts

--- week-02/day-02/data-structures.ts
+++ week-02/day-02/data-structures.ts
@@ -1,5 +1,6 @@
 
 //List introduction 1
+/*
 {
 let names: string[] = [];
 
@@ -7,7 +8,9 @@ console.log(names);
 
 names.push(`William`)
 
-console.log(`List has at least one element in it: `, names != undefined);
+if(Object.entries(names).length === 0){
+console.log(`This list is empty`)
+}
 
 names.push(`John`)
 names.push(`Amanda`)
@@ -26,7 +29,6 @@ names.forEach(listing2);
 function listing2(index: number, item: string){
     console.log((index), item);
 }
-*/
 
 names.splice(names.indexOf(names[1]), 1);
 
@@ -43,5 +45,82 @@ names = [];
 
 console.log(names);
 }
+*/
 
+/*
+//Map introduction 1
+
+let oshiiMap = new Map();
+
+if (oshiiMap.size === 0) {
+    console.log(`This map is empty`)
+}
+else {
+    console.log(`This map has something in it`);
+}
+
+oshiiMap.set(97, `a`)
+    .set(98, `b`)
+    .set(99, `c`)
+    .set(65, `A`)
+    .set(66, `B`)
+    .set(67, `C`);
+
+for (let [key] of oshiiMap.entries()) {
+    console.log(key);
+}
+
+let oshiiValue = oshiiMap.values();
+for (let i = 0; i < oshiiMap.size; i++) {
+    console.log(oshiiValue.next().value);
+}
+
+//Printing keys and values
+//for (let [key, value] of oshiiMap.entries()) {
+//    console.log(key + ` = ` + value);
+//}
+
+oshiiMap.set(68, `D`);
+
+console.log(oshiiMap.size)
+
+console.log(oshiiMap.get(99));
+
+oshiiMap.delete(97)
+
+console.log(oshiiMap.has(100));
+
+//oshiiMap.clear
+*/
+
+//List introduction 2
+/*
+let ListA: string[] = ['Apple', 'Avocado', 'Blueberries', 'Durian', 'Lychee'];
+let ListB: string[] = ListA.slice();
+
+console.log(ListA.includes(`Durian`));
+
+ListB.splice(ListB.indexOf(`Durian`), 1);
+console.log(ListB);
+
+ListA.splice(4, 0, `Kiwifruit`);
+
+if (ListA.length > ListB.length) {
+    console.log(`Az A lista hosszabb.`);
+}
+else {
+    console.log(`A B lista hosszabb`);
+}
+
+console.log(ListA.indexOf(`Avocado`));
+
+console.log(ListB.indexOf(`Durian`));
+
+ListB.push(`Passion Fruit`, `Pomelo`)
+
+console.log(ListA[2]);
+
+*/
+
+//Map introduction 2
 
