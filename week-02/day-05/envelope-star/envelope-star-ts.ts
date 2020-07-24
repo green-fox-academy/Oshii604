aa892ab3d1c28b5b2ea7cd2576fe343c2707f66f
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-24 14:33:23 +0200

    envelope star 75 percent ready, but it's a dead-end

--- week-02/day-05/envelope-star/envelope-star-ts.ts
+++ week-02/day-05/envelope-star/envelope-star-ts.ts
@@ -9,9 +9,9 @@ let y = canvas.width / 2 - 20;
 let a = canvas.width / 2;
 let b = 0;
 
-let drawPurpleLine = function () {
+let drawHalfOne = function () {
     for (let i = 0; i < 2; i++) {
-        for (let i = 0; i < 15; i++) {
+        for (let j = 0; j < 15; j++) {
             ctx.beginPath();
             ctx.moveTo(b + x, a);
             ctx.lineTo(a, y);
@@ -28,5 +28,31 @@ let drawPurpleLine = function () {
     }
 }
 
-drawPurpleLine()
+drawHalfOne()
 
+
+
+a = canvas.width/2;
+x = 0;
+b = canvas.width/2;
+y= 0;
+let drawHalfTwo = function () {
+    for (let i = 0; i < 1; i++) {
+       
+        for (let j = 0; j < 15; j++) {
+            ctx.beginPath();
+            ctx.moveTo(b + x, a);
+            ctx.lineTo(a, y);
+            ctx.strokeStyle = `lime`;
+            ctx.stroke();
+            y += 20;
+            x += 20;
+        }
+
+
+
+    }
+}
+
+
+drawHalfTwo()
