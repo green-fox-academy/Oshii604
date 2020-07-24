135f4af2422a30acab898bc44653b20b01ff2a95
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-24 10:26:36 +0200

    day05 folder created, mods on checkerboard.ts

---
+++ week-02/day-04/drawing/checkerboard/checkerboard-ts.ts
@@ -0,0 +1,35 @@
+
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export { };
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+function drawSquare(x: number) {
+
+    let v = 0;
+    let h = x * (-8);
+
+    for (let i = 8; i > 0; i--) {
+        for (let j = 0; j < 16; j++) {
+            if (j % 2 == 0) {
+                ctx.fillStyle = `black`;
+                ctx.fillRect(h, v, x, x,)
+            }
+            else {
+                ctx.fillStyle = `white`;
+                ctx.fillRect(h, v, x, x,)
+            }
+            h += x;
+        }
+        h = x * (-i + 1)
+        v += x;
+    }
+}
+
+drawSquare(100)
+
+
+// Fill the canvas with a checkerboard pattern.
