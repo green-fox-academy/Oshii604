b56e0e03724e08c17c7e2997e0ea535a6a8568a6
Laszlo Osvath <oshii.oshii@gmail.com>
2020-07-27 12:26:20 +0200

    post-it.ts added

--- week-03/day-01/post-it.ts
+++ week-03/day-01/post-it.ts
@@ -1,16 +1,5 @@
 `use strict`;
 
-/*
-Create a PostIt a class that has
-a backgroundColor
-a text on it
-a textColor
-Create a few example post-it objects:
-an orange with blue text: "Idea 1"
-a pink with black text: "Awesome"
-a yellow with green text: "Superb!"
-*/
-
 export default class PostIt {
     backgroundColor: string;
     text: string;
@@ -21,9 +10,9 @@ export default class PostIt {
         this.text = text;
         this.textColor = textColor;
     }
-    getPostIt(){
+    getPostIt() {
         console.log(`Background color is ${this.backgroundColor}, the text on it is "${this.text}", and it's written with ${this.textColor} color!`);
-        
+
     }
 }
 let orangePostIt = new PostIt(`orange`, `Idea1`, `blue`)
@@ -33,3 +22,5 @@ let yellowPostIt = new PostIt(`yellow`, `Superb!`, `green`)
 orangePostIt.getPostIt();
 pinkPostIt.getPostIt();
 yellowPostIt.getPostIt();
+
+console.log(orangePostIt, pinkPostIt, yellowPostIt);
